7dd49772443582eadf45e033c1b0a071c35a9283
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-01 14:38:15 -0700

    notes from demo

---
+++ test/clients.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const Clients = require('../lib/clients');
+
+describe('Clients', () => {
+    let clients = null;
+    beforeEach(() => {
+        clients = new Clients();
+    });
+
+    it('assigns names', () => {
+        const c1 = clients.add({});
+        const c2 = clients.add({});
+        const c3 = clients.add({});
+
+        assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
+    });
+
+    it('remove a client', () => {
+        const c1 = clients.add({});
+        clients.remove(c1);
+
+        assert(!clients.set.has(c1));
+    });
+});
