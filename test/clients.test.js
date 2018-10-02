0c32b01195428648f2702dc53690fe3cdaad3c51
Sophie Graham <sophiergraham1@gmail.com>
2018-10-02 12:50:01 -0700

    fixed linter error

--- test/clients.test.js
+++ test/clients.test.js
@@ -17,5 +17,5 @@ describe('Clients', () => {
         assert.equal(c1.username, 'user1');
         assert.equal(c2.username, 'user2');
         assert.equal(c3.username, 'user3');
-    })
+    });
 });
