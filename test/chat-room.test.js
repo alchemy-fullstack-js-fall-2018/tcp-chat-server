6418edb5d45c35022321ad8d320cbfcf05298bd6
KLPainter <KLPainter@gmail.com>
2018-10-01 17:43:58 -0700

    parse message tests failing

---
+++ test/chat-room.test.js
@@ -0,0 +1,20 @@
+//const assert = require('assert');
+//const chatRoom = require('../lib/chat-room.js');
+
+// describe('chat room', () => {
+
+//     it('takes a socket, assigns user name, and stores by user name', () => {
+//         const chatRoom = new chatRoom();
+        
+//     });
+
+//     /*
+//     Test that a ChatRoom instance 'takes a socket, assigns random user name, and stores by user name'. Do this by
+// creating a new ChatRoom
+// call .add(client) (note you can pass in a "mock", a plain empty object. Doesn't have to be a socket instance)
+// assert that a name was returned as the username property of the client object (e.g. assert.equal(client.username, username);)
+// assert that the name was assigned as a property to the client object
+// call .getClient(username) passing in the username returned from the .add call, and assert that the returned object is the same as what you supplied to the .add call.
+//     */
+
+// });
---
+++ test/parse-message.test.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const parseMessage = require('../lib/parse-message.js');
+
+describe('parse message', () => {
+
+    const msgBroadcastAll = '@all hello world!';
+    const msgDirectMessage = '@dm:exampleUser Hello this is a direct message!';
+    const msgChangeName = '@nick:exampleUser';
+
+    it('returns null when passed a string that does not start with @', () => {
+        const result = parseMessage('all hello world!');
+        const expected = null;
+        assert.equal(result, expected);
+    });
+
+    it('returns an object with no arg for @all', () => {
+        const result = parseMessage('@all hello world');
+        const expected = {
+            command: 'all',
+            arg: '',
+            text: 'hello world'
+        };
+        assert.deepEqual(result, expected);
+    });
+
+    it('returns a complete object for @dm', () => {
+        const result = parseMessage('@dm:exampleUser Hello this is a direct message!');
+        const expected = {
+            command: 'dm',
+            arg: 'exampleUser',
+            text: 'Hello this is a direct message!'
+        };
+        assert.deepEqual(result, expected);
+    });
+
+    it('returns an object with no text for @nick', () => {
+        const result = parseMessage('@nick:exampleUser');
+        const expected = {
+            command: 'nick',
+            arg: 'exampleUser',
+            text: ''
+        };
+        assert.deepEqual(result, expected);
+    });
+
+});
