692290d77a1844ac4bb6abc48e89949721ba2f6e
Sophie Graham <sophiergraham1@gmail.com>
2018-10-02 12:00:20 -0700

    starting step two

--- test/clients.test.js
+++ test/clients.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const Clients = require('../lib/clients');
+const Clients = require('../lib/chatroom');
 
 describe('Clients', () => {
 
@@ -13,4 +13,9 @@ describe('Clients', () => {
         clients.add(c2);
         clients.add(c3);
     });
+    it('assigns names', () => {
+        assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
+    })
 });
