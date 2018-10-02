e353ff60c2872793b5c33646f03158b6581b978c
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-01 18:11:13 -0700

    all tests working but rename failing

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -21,4 +21,29 @@ describe('Chatroom', () => {
         assert.equal(c3.username, 'user3');
     });
 
+    it('gets a client', () => {
+        const client = chatRoom.getClient(c1.username);
+        assert.deepEqual(client, c1);
+    });
+
+    it('it renames a user', () => {
+        const client = c1.username;
+        const newName = 'I-Love-ALC';
+        const newusername = chatRoom.rename(client, newName);
+        assert.equal(newusername, true);
+        assert.equal(chatRoom.getClient(client), undefined);
+        assert.ok(chatRoom.getClient(newName));
+        assert.equal(chatRoom.getClient(newName).username, newName);
+    });
+
+    it('stores clients', () => {
+        const allClients = chatRoom.all();
+        assert.deepEqual(allClients, [c1, c2, c3]);
+    });
+
+    it('lists of client minus sender', () => {
+        const broadcast = chatRoom.getBroadcastClients(c1);
+        assert.deepEqual(broadcast, [c2, c3]);
+    });
+
 });
