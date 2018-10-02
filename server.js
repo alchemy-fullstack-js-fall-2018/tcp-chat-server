85654c6cfd9670c2abe7fa7aabc52a7b1b009413
Davidchhing <davidhchhing@gmail.com>
2018-10-02 10:29:04 -0700

    rearranging files

---
+++ server.js
@@ -0,0 +1,3 @@
+const net = require('net');
+const app = require('./lib/app');
+const port = 8000;
--- test/client.test.js
+++ test/client.test.js
@@ -3,17 +3,38 @@ const Clients = require('./lib/client');
 
 describe('Clients', () => {
     
-    const users = [];
+    const c1 = {};
+    const c2 = {};
+    const c3 = {};
     let clients = null;
 
     beforeEach(() => {
         clients = new Clients();
-        clients.add([]);
+        clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
     });
 
     it('assigns a user name', () => {
-        assert.equal(this.clients, )
+        assert.equal(c1.clients, 'user1');
+        assert.equal(c1.clients, 'user2');
+        assert.equal(c1.clients, 'user3');
     });
 
-    
+    it('stores a client', () => {
+        const allClients = clients.getAllClients();
+        assert.deepEqual(allClients, [c1, c2, c3]);
+    });
+
+    it('removes a client', () => {
+        clients.remove(c1);
+        const allClients = clients.getAllClients();
+        assert.deepEqual(allClients, [c2, c3]);
+
+    });
+
+    it('gives back list of clients (minus sender)', () => {
+        const broadcast = clients.getBroadcastClients(c1);
+        assert.deepEqual(broadcast, [c2, c3]);
+    });
 });
