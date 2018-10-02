742d87a0a32df11f266c4b58ab8208b05d0f1715
Davidchhing <davidhchhing@gmail.com>
2018-10-01 17:57:44 -0700

    set up server in app file and started writing client.test

---
+++ test/client.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const Clients = require('./lib/client');
+
+describe('Clients', () => {
+    
+    const users = [];
+    let clients = null;
+
+    beforeEach(() => {
+        clients = new Clients();
+    });
+
+    it('assigns a user name', () => {
+        assert.equal(this.clients, )
+    });
+
+    
+});
