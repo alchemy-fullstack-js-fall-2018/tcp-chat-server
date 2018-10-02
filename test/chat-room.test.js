25c4b031424ad9bc680c43e5c039c87748aa26f4
KLPainter <KLPainter@gmail.com>
2018-10-02 10:01:38 -0700

    third chat room test passes

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -5,40 +5,33 @@ describe('chat room', () => {
 
     it('takes a socket, assigns user name, and stores by user name', () => {
         const chatRoom = new ChatRoom();
-        let client = {};
-        let userName = chatRoom.add(client);
+        const client = {};
+        const userName = chatRoom.add(client);
         assert.equal (client.userName, 'user1');
-        let returnedClient = chatRoom.getClient(userName);
+        const returnedClient = chatRoom.getClient(userName);
         assert.deepEqual(returnedClient, client);        
     });
 
-    /*
-    Test that a ChatRoom instance 'takes a socket, assigns random user name, and stores by user name'. Do this by
-creating a new ChatRoom
-call .add(client) (note you can pass in a "mock", a plain empty object. Doesn't have to be a socket instance)
-assert that a name was returned as the username property of the client object (e.g. assert.equal(client.username, username);)
-assert that the name was assigned as a property to the client object
-call .getClient(username) passing in the username returned from the .add call, and assert that the returned object is the same as what you supplied to the .add call.
-    */
-
-    it.skip('', () => {
-        const chatRoom = new chatRoom();
-    
+    it('rename works properly', () => {
+        const chatRoom = new ChatRoom();
+        const client = {};
+        const userName = chatRoom.add(client);
+        const result = chatRoom.rename(userName, 'newUserName');
+        assert.ok(result);
+        const returnedClient = chatRoom.getClient(userName);
+        assert.equal(returnedClient, undefined);
+        assert.equal(client.userName, 'newUserName');
     });
 
-    /*
-Test that a ChatRoom instance 'renames a user'
-Create a new ChatRoom and add a user as above
-Call .rename(username, newusername)
-assert that the call to .rename returns true
-assert that using the old username does not return the client
-assert that using the new username does return the client
-assert that the client.username is now equal to the new username (This means the property on the client object needs to change as well as the key in the map.
-*/
-
-    it.skip('', () => {
-        const chatRoom = new chatRoom();
-    
+    it('can not rename to existing user name', () => {
+        const chatRoom = new ChatRoom();
+        const client1 = {}, client2 = {};
+        const userName1 = chatRoom.add(client1);
+        const userName2 = chatRoom.add(client2);
+        const result = chatRoom.rename(userName1, userName2);
+        assert.ok(!result);
+        assert.deepEqual(chatRoom.getClient(userName1), client1);
+        assert.deepEqual(chatRoom.getClient(userName2), client2);
     });
 
     /*
@@ -50,7 +43,7 @@ assert that username1 and username2 return their original client objects
 */
 
     it.skip('', () => {
-        const chatRoom = new chatRoom();
+        const chatRoom = new ChatRoom();
     
     });
 
