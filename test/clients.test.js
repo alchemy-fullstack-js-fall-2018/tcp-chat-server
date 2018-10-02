8feba89e3dcb463f7972611974a28e2e7ba7e445
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-01 18:05:35 -0700

    nothing works

--- test/clients.test.js
+++ test/clients.test.js
@@ -3,35 +3,38 @@ const Clients = require('../lib/clients');
 
 describe('Clients', () => {
     let clients = null;
+    
     beforeEach(() => {
         clients = new Clients();
+        clients.add('foo');
+        console.log('clients', clients);
+
     });
 
     it('assigns names', () => {
-        const c1 = clients.add({});
-        const c2 = clients.add({});
-        const c3 = clients.add({});
+        assert.equal(clients, 'foo');
 
-        assert.equal(c1.username, 'user1');
-        assert.equal(c2.username, 'user2');
-        assert.equal(c3.username, 'user3');
     });
 
-    it('remove a client', () => {
-        const c1 = clients.add({});
-        clients.remove(c1);
+    // it('remove a client', () => {
+    //     const c1 = clients.add({});
+    //     clients.remove(c1);
 
-        assert(!clients.set.has(c1));
-    });
+    //     assert(!clients.set.has(c1));
+    // });
 
-    it('gets all clients', () => {
-        const c1 = clients.add({});
-        const c2 = clients.add({});
-        const c3 = clients.add({});
-        const expectedClients = [c1, c2, c3];
+    // it('gets all clients', () => {
 
-        const allClients = clients.getAll();
+    //     const expectedClients = [c1];
 
-        assert.deepEqual(allClients, expectedClients);
-    });
+    //     const allClients = clients.getAll();
+
+    //     assert.deepEqual(allClients, expectedClients);
+    // });
+
+    // it('gets all clients except self', () =>{
+    //     const broadcastClients = clients.getBroadcastClients();
+    //     assert.deepEqual(broadcastClients, )
+    // })
+
 });
