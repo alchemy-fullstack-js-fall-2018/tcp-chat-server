727f58a0311c8a598ea4f6ea2e7494a41858451a
Ryan Luras <ryanluras@gmail.com>
2018-10-02 13:55:45 -0700

    rename working

--- test/clients.test.js
+++ test/clients.test.js
@@ -15,9 +15,9 @@ describe('Clients', () => {
     });
 
     it('assigns names', () => {
-        assert.equal(c1.username, 'user1');
-        assert.equal(c2.username, 'user2');
-        assert.equal(c3.username, 'user3');
+        assert.equal(c1.userName, 'user1');
+        assert.equal(c2.userName, 'user2');
+        assert.equal(c3.userName, 'user3');
     });
 
     it('stores clients', () => {
@@ -36,4 +36,15 @@ describe('Clients', () => {
         const broadcast = clients.getBroadcastClients(c1);
         assert.deepEqual(broadcast, [c2, c3]);
     });
+
+    it('rename works properly', () => {
+        const clients = new Clients();
+        const c1 = {};
+        clients.add(c1);
+        newUserName = 'bob';
+        const result = clients.rename(c1.userName, newUserName);
+        assert.ok(result);
+        assert.equal(c1.userName, newUserName);
+    });
+
 });
