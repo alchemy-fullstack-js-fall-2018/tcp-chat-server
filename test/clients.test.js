78f67dec508a485aa14cff3be5de9830c2330a7c
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-02 10:03:47 -0700

    getAllClients passing... not sure if it's what I want

--- test/clients.test.js
+++ test/clients.test.js
@@ -15,7 +15,18 @@ describe('Clients', () => {
 
     it('assigns names', () => {
         assert.equal(c1.username, 'user1');
-        
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');        
     });
+
+    it('will store all the clients', () => {
+        const allClients = clients.getAllClients();
+        assert.deepEqual(allClients, [c1, c2, c3]);
+    });
+
+    // it('removes a client', () => {
+    //     clients.remove(c1);
+    //     const all
+    // });
 
 });
