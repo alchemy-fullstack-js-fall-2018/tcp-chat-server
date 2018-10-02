5b49fd58193e423783fd5bcfd7c5edd5dc39a771
Sophie Graham <sophiergraham1@gmail.com>
2018-10-02 10:37:46 -0700

    have set up and started client js + client js test

---
+++ test/clients.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const Clients = require('../lib/clients');
+
+describe('Clients', () => {
+
+    const c1 = {};
+    const c2 = {};
+    const c3 = {};
+    let clients = null;
+    beforeEach(() => {
+        clients = new Clients();
+        clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
+    });
+});
