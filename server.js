89a73e588ae4efb9966fe6180add1112b80e4c43
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-02 11:42:39 -0700

    tcp chatroom working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./lib/app');
 const PORT = 15678;
 
 app.on('listening', () => {
+    //eslint-disable-next-line no-console
     console.log('app started on port', PORT);
 });
 
--- test/parse-message.test.js
+++ test/parse-message.test.js
@@ -2,18 +2,39 @@ const assert = require('assert');
 const parseMessage = require('../lib/parse-message');
 
 describe('parse message', () => {
-    const failure = 'hello';
-    const correct = '@all hello';
-    const expected = {
-        cmd: 'all',
-        arg: undefined,
-        text: 'hello'
-    };
+    
     it('returns null if no "@" is supplied', () => {
+        const failure = 'hello';
         assert.equal(parseMessage(failure), null);
     });
 
     it('returns an object', () => {
+        const correct = '@all hello';
+        const expected = {
+            cmd: 'all',
+            arg: undefined,
+            text: 'hello'
+        };
+        assert.deepEqual(parseMessage(correct), expected);
+    });
+
+    it('handles @nick', () => {
+        const correct = '@nick:coolguy';
+        const expected = {
+            cmd: 'nick',
+            arg: 'coolguy',
+            text: ''
+        };
+        assert.deepEqual(parseMessage(correct), expected);
+    });
+
+    it('handles @dm', () => {
+        const correct = '@dm:coolguy what up bro?';
+        const expected = {
+            cmd: 'dm',
+            arg: 'coolguy',
+            text: 'what up bro?'
+        };
         assert.deepEqual(parseMessage(correct), expected);
     });
 
