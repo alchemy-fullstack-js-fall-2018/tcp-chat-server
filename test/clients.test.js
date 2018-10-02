80d7ac07469f91caad0de9625c5d8741a2990606
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-02 11:09:40 -0700

    refactored add, getall, remove

--- test/clients.test.js
+++ test/clients.test.js
@@ -21,13 +21,13 @@ describe('Clients', () => {
 
     it('will store all the clients', () => {
         const allClients = clients.getAllClients();
-        assert.deepEqual(allClients, [c1, c2, c3]);
+        assert.deepEqual(allClients, [c1.username, c2.username, c3.username]);
     });
 
-    it('removes a client', () => {
-        clients.remove(c1);
+    it.only('removes a client', () => {
+        clients.remove(c1.username);
         const allClients = clients.getAllClients();
-        assert.deepEqual(allClients, [c2, c3]);
+        assert.deepEqual(allClients, [c2.username, c3.username]);
     });
 
     it('will give a list of clients other than the sender', () => {
