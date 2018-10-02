131859add7821e07c5c291c3042dedec70a4e036
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-02 09:26:37 -0700

    first test passing for clients using set

---
+++ test/clients.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const Clients = require('../lib/clients');
+
+describe('Clients', () => {
+    const c1 = {};
+    const c2 = {};
+    const c3 = {};
+    let clients = null;
+    beforeEach(() => {
+        clients = new Clients();
+        clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
+    });
+
+    it('assigns names', () => {
+        assert.equal(c1.username, 'user1');
+        
+    });
+
+});
