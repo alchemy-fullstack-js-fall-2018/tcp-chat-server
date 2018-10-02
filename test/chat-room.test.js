09fdd1dbfb4dd64896f02df0e43de6684316e031
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-01 17:17:42 -0700

    add test passing

--- test/chat-room.test.js
+++ test/chat-room.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const ChatRoom = require('../lib/chatRoom');
+const ChatRoom = require('../lib/chat-room');
 
 describe('Chatroom', () => {
 
@@ -13,6 +13,12 @@ describe('Chatroom', () => {
         chatRoom.add(c1);
         chatRoom.add(c2);
         chatRoom.add(c3);
+    });
+
+    it('assigns names to user objects', () => {
+        assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
     });
 
 });
