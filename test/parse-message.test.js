535afb1415df83a13b4d93fdd4e654e9f97ffaae
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-02 09:33:56 -0700

    parse message passing tests

--- test/parse-message.test.js
+++ test/parse-message.test.js
@@ -3,8 +3,18 @@ const parseMessage = require('../lib/parse-message');
 
 describe('parse message', () => {
     const failure = 'hello';
+    const correct = '@all hello';
+    const expected = {
+        cmd: 'all',
+        arg: undefined,
+        text: 'hello'
+    };
     it('returns null if no "@" is supplied', () => {
         assert.equal(parseMessage(failure), null);
+    });
+
+    it('returns an object', () => {
+        assert.deepEqual(parseMessage(correct), expected);
     });
 
 });
