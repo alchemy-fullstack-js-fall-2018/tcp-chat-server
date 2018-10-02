4bff3eb17293fd5b902c748df6b49bde32e2863c
MikeBLambert <mlamb128@gmail.com>
2018-10-02 12:09:42 -0700

    Can broadcast messages to all users

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-// const dgram = require('dgram');
 const app = require('./lib/app');
 
 app.listen(9876);
-// const server = dgram.createSocket('udp4');
 
-// server.on('error', (err) => {
-//     console.log(`server error: \n${err.stack}`);
-//     server.close();
-// });
-
-// server.on('listening', () => {
-//     const address = server.address();
-//     console.log(`server listening ${address.address}:${address.port}`);
-// });
-
-// server.bind(9876);
