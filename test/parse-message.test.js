486415e396912f37287fcc32d0fe9a5e95748d77
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-07 12:10:16 -0700

    message parsing test passing

--- test/parse-message.test.js
+++ test/parse-message.test.js
@@ -6,4 +6,14 @@ describe('parseMessage', () => {
     it('only accepts messages that start with "@"', () => {
         assert.equal(parseMessage('ham is delicious'), null);
     });
+
+    it('returns the message as an object broken into parts', () => {
+        const msg = '@command:parameter text';
+        const expected = {
+            cmd: 'command',
+            arg: 'parameter',
+            text: 'text'
+        };
+        assert.deepEqual(parseMessage(msg), expected);
+    });
 });
