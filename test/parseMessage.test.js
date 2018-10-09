1b5d9e57e7a094ff835971e77742d8309401c68a
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-09 16:41:44 -0700

    2nd parser test green

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -5,4 +5,11 @@ describe('parseMessage', () => {
     it('returns the message if @ isn\'t the first character', () => {
         assert.equal(parseMessage('hello world'), 'hello world');
     });
+    it('returns an object with command, arg, and text when running something like: command:arg text', () => {
+        assert.deepEqual(parseMessage('@nick:sarah I am changing my name'), {
+            command: '@nick',
+            arg: 'sarah',
+            text: 'I am changing my name'
+        });
+    });
 });
