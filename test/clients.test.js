513036034037b785bc71161368b2366dc5fe26a5
Sophie Graham <sophiergraham1@gmail.com>
2018-10-09 18:08:11 -0700

    finished tests

--- test/clients.test.js
+++ test/clients.test.js
@@ -6,16 +6,37 @@ describe('Clients', () => {
     const c1 = {};
     const c2 = {};
     const c3 = {};
+
     let clients = null;
+
     beforeEach(() => {
         clients = new Clients();
         clients.add(c1);
         clients.add(c2);
         clients.add(c3);
     });
+
     it('assigns names', () => {
         assert.equal(c1.username, 'user1');
         assert.equal(c2.username, 'user2');
         assert.equal(c3.username, 'user3');
     });
+
+    it('remove a client', () => {
+        clients.remove(c1);
+        assert(!clients.map.has(c1.username));
+    });
+
+    it('gets all clients', () => {
+        const expectedClients = [c1, c2, c3];
+        const allClients = clients.getAllClients();
+
+        assert.deepEqual(allClients, expectedClients);
+    });
+
+    it('gets all clients except self', () => {
+        const broadcastClients = clients.getBroadcastClients(c1);
+        assert.deepEqual(broadcastClients, [c2, c3]);
+    });
+
 });
--- test/shout.test.js
+++ test/shout.test.js
@@ -1,1 +1,14 @@
-// const assert = require('assert');
+const assert = require('assert');
+const processMessage = require('../lib/processMessage');
+
+describe('shout message', () => {
+
+    it('ignores strings that do not start with @', () => {
+        const message = processMessage('hello');
+        assert.equal(message, null);
+    });
+    it('est that a string returns an object', () => {
+        const message = processMessage('@cmd:param some text');
+        assert.deepEqual(message, { command: 'cmd', arg: 'param', text: 'some text' });
+    });
+});
